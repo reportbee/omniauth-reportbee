a9820a9378e30a71c534540ffd05e32cf01549cd
Sudhanshu <sudhanshu.kumar@in.reportbee.com>
2016-05-13 14:51:54 +0530

    updated according to code review comments.

--- lib/omniauth/reportbee.rb
+++ lib/omniauth/reportbee.rb
@@ -2,17 +2,24 @@ require 'rest-client'
 
 module OmniAuth
   class Reportbee
-    def self.do_api_call( api_url_string, access_token, source_params = {}, post_request = false )
+    def self.do_api_call( api_url_string, access_token_string, source_params = {}, post_request = false )
       # encode api_url_string to url string, because api_url_string is normal string here with spaces and special characters also.
-      url_string = URI.encode(api_url_string)
+      url_string = URI.encode( api_url_string )
+
+      access_token_string = get_client_credentials_access_token if access_token_string.blank?
+      access_token_object = get_access_token_object( access_token_string )
 
       header_hash = { 'Accept' => 'reportbee/esf.version.v2' }
       options = { body: source_params, headers: header_hash }
 
       # do api call
-      response_hash = post_request ? access_token.post( url_string, options ) : access_token.get( url_string, options )
+      response_hash = post_request ? access_token_object.post( url_string, options ) : access_token_object.get( url_string, options )
 
-      response_hash.parsed
+      [ response_hash.parsed, access_token_string ]
+    rescue => e
+      response_hash = false_response_hash("Something went wrong while doing api calls! #{e.message}")
+
+      [ response_hash, access_token_string ]
     end
 
     def self.get_client_credentials_access_token
@@ -27,10 +34,15 @@ module OmniAuth
       response_hash['access_token']
     end
 
-    def self.get_access_token_object( access_token )
+    def self.get_access_token_object( access_token_string )
       oauth_client = ::OAuth2::Client.new( ENV['OAUTH_APP_ID'], ENV['OAUTH_APP_SECRET'], :site => ENV['OAUTH_APP_URL'] )
 
-      ::OAuth2::AccessToken.new( oauth_client, access_token )
+      ::OAuth2::AccessToken.new( oauth_client, access_token_string )
+    end
+
+    def self.false_response_hash( message = 'All parameters not sent' )
+      status = false
+      { 'status' => status, 'message' => message }
     end
   end
 end
