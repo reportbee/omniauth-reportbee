eb7e70ae940ebc07b339e1a90f3de3963bb994d1
Anjan <anjan@reportbee.com>
2016-08-30 13:59:05 +0530

    1. Adding airbrake as a dependency and sending an airbrake notification when an exception occurs. 2. Changed the method params which sends API calls. Now, it accepts a hash instead a long list of params. In the hash, it accepts an access_token object and not a string. It is the responsibility of the calling method to build the access token object. Doing this, so we don't automatically fall back to client credentials flow just because access token string is not sent. "Which flow to use" should be controlled by the caller and not left to the liberty of this method. 3. When building an access token object, automatically checking if the token is expired and refreshing it, if needed. 4. Also added a method to handle "App-to-App" calls. This is used when communicating between one client app to another. This method sends the access token string to the receiving client app. The receiving client app verifies the access token with the Doorkeeper server running on RB. Then it executes the actual API, if the AT is valid. 5. Also rescuing from OAuth2::Error, because this is what gets thrown when a 401 unauthorised gets returned by the OAuth2 server.

--- lib/omniauth/reportbee.rb
+++ lib/omniauth/reportbee.rb
@@ -1,50 +1,133 @@
 require 'rest-client'
+begin
+  require 'airbrake'
+rescue LoadError
+  warn 'Install airbrake gem to correctly log exceptions.'
+end
 
 module OmniAuth
   class Reportbee
-    def self.do_api_call( api_url_string, access_token_string, source_params = {}, post_request = false )
-      # encode api_url_string to url string, because api_url_string is normal string here with spaces and special characters also.
-      url_string = URI.encode( api_url_string )
+    class << self
+      def do_esf_api_call( args )
+        api_response_hash = {}
+        api_url_string = args[:api_url_string]
+        access_token_object = args[:access_token_object]
+        raise 'No access token object received. Cannot make API call.' if access_token_object.blank?
 
-      access_token_string = get_client_credentials_access_token if access_token_string.blank?
-      access_token_object = get_access_token_object( access_token_string )
+        source_params = args.fetch(:source_params, {})
+        is_post_request = args.fetch(:is_post_request, false)
 
-      header_hash = { 'Accept' => 'reportbee/esf.version.v2' }
-      options = { body: source_params, headers: header_hash }
+        # encode api_url_string to url string, because api_url_string is normal string here with spaces and special characters also.
+        url_string = URI.encode( api_url_string )
 
-      # do api call
-      api_response_hash = post_request ? access_token_object.post( url_string, options ) : access_token_object.get( url_string, options )
-      response_hash = { response_hash: api_response_hash.parsed, access_token_string: access_token_string }
+        header_hash = { 'Accept' => 'reportbee/esf.version.v2' }
+        options = { body: source_params, headers: header_hash }
 
-      response_hash
-    rescue => e
-      api_response_hash = false_response_hash("Something went wrong while doing api calls! #{e.message}")
-      response_hash = { response_hash: api_response_hash, access_token_string: access_token_string }
+        # do api call
+        api_response_hash = is_post_request ? access_token_object.post( url_string, options ) : access_token_object.get( url_string, options )
+        api_response_hash.parsed
+      rescue ::OAuth2::Error => e
+        error_message = "OAuth2 Error while Making ESF API call from omniauth-rb gem. API URL: #{api_url_string}. Error message: #{e.message}. Error code: innutritious."
 
-      response_hash
-    end
+        if defined?( Rails ) && Rails.env.development?
+          puts '===================== Exception occurred ====================='
+          puts error_message
+          puts '=============================================================='
+        else
+          if defined?( Airbrake ) && ::Airbrake.send(:configured?, :default)
+            ab_hash = { error_class: 'ESF API Call', error_message: error_message, url_string: api_url_string,
+                        error_code: e.code, error_description: e.description, response_hash: e.response }
+            ::Airbrake.notify( e, ab_hash )
+          end
+        end
 
-    def self.get_client_credentials_access_token
-      url = ENV['OAUTH_APP_URL'] + '/oauth/token'
-      client_id = ENV['OAUTH_APP_ID']
-      client_secret = ENV['OAUTH_APP_SECRET']
+        false_response_hash( error_message )
+      rescue StandardError => e
+        error_message = "Error while Making ESF API call from omniauth-rb gem. API URL: #{api_url_string}. Error message: #{e.message}. Error code: unfunniness."
 
-      options = { grant_type: 'client_credentials', client_id: client_id, client_secret: client_secret }
-      response = RestClient.post( url, options )
+        if defined?( Rails ) && Rails.env.development?
+          puts '===================== Exception occurred ====================='
+          puts error_message
+          puts '=============================================================='
+        else
+          if defined?( Airbrake ) && ::Airbrake.send(:configured?, :default)
+            ab_hash = { error_class: 'ESF API Call', error_message: error_message, url_string: api_url_string, response_hash: api_response_hash }
+            ::Airbrake.notify( e, ab_hash )
+          end
+        end
 
-      response_hash = JSON.parse( response )
-      response_hash['access_token']
-    end
+        false_response_hash( error_message )
+      end
 
-    def self.get_access_token_object( access_token_string )
-      oauth_client = ::OAuth2::Client.new( ENV['OAUTH_APP_ID'], ENV['OAUTH_APP_SECRET'], :site => ENV['OAUTH_APP_URL'] )
+      def do_app2app_api_call( args )
+        response = {}
+        api_url_string = args[:api_url_string]
+        access_token_object = args[:access_token_object]
+        raise 'No access token object received. Cannot make API call.' if access_token_object.blank?
 
-      ::OAuth2::AccessToken.new( oauth_client, access_token_string )
-    end
+        source_params = args.fetch(:source_params, {})
+        is_post_request = args.fetch(:is_post_request, false)
 
-    def self.false_response_hash( message = 'All parameters not sent' )
-      status = false
-      { 'status' => status, 'message' => message }
+        source_params.merge!( access_token: access_token_object.token )
+
+        api_url = URI.escape( api_url_string )
+        uri = URI.parse( api_url )
+
+        http = Net::HTTP.new( uri.host, uri.port )
+
+        if 'https' == APP_CONFIG[:protocol] || api_url_string.include?( 'https' )
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+
+        request = is_post_request ? Net::HTTP::Post.new(uri.request_uri) : Net::HTTP::Get.new( uri.request_uri )
+        request.set_form_data( source_params )
+        request['Accept'] = 'reportbee/esf.version.v2'
+        # If debugging of requests are required, then enable the following line.
+        # http.set_debug_output($stdout) if Rails.env.development?
+
+        response = http.request( request )
+
+        raise 'No response received' unless response.present?
+        raise "Wrong response code #{response.code} received" unless '200' == response.code
+
+        response.body
+      rescue StandardError => e
+        error_message = "Error while Making App2App API call from omniauth-rb gem. API URL: #{api_url_string}. Error message: #{e.message}. Error code: pedestaled."
+
+        if defined?( Rails ) && Rails.env.development?
+          puts '===================== Exception occurred ====================='
+          puts error_message
+          puts '=============================================================='
+        else
+          if defined?( Airbrake ) && ::Airbrake.send(:configured?, :default)
+            ab_hash = { error_class: 'App2App API Call', error_message: error_message, url_string: api_url_string, response_hash: response }
+            ::Airbrake.notify( e, ab_hash )
+          end
+        end
+
+        false_response_hash( error_message )
+      end
+
+      def get_client_credentials_access_token_object
+        oauth_client.client_credentials.get_token
+      end
+
+      def get_access_token_object( access_token_string, refresh_token_string, expires_at )
+        acc_token = ::OAuth2::AccessToken.new( oauth_client, access_token_string, {refresh_token: refresh_token_string, expires_at: expires_at} )
+        acc_token.expired? ? acc_token.refresh! : acc_token
+      end
+
+      private
+
+      def oauth_client
+        @oauth_client ||= ::OAuth2::Client.new( ENV['OAUTH_APP_ID'], ENV['OAUTH_APP_SECRET'], :site => ENV['OAUTH_APP_URL'] )
+      end
+
+      def false_response_hash( message = 'All parameters not sent' )
+        status = false
+        { 'status' => status, 'message' => message }
+      end
     end
   end
 end
--- omniauth-reportbee.gemspec
+++ omniauth-reportbee.gemspec
@@ -4,31 +4,32 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'omniauth-reportbee/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "omniauth-reportbee"
+  spec.name          = 'omniauth-reportbee'
   spec.version       = Omniauth::Reportbee::VERSION
-  spec.authors       = ["Prashanth"]
-  spec.email         = ["prashanth.r@in.reportbee.com"]
+  spec.authors       = ['Report Bee']
+  spec.email         = ['admin@reportbee.com']
 
-  spec.summary       = %q{Reportbee omniauth gem }
+  spec.summary       = %q{Report Bee omniauth gem }
   spec.description   = %q{Implements omniauth functionality for clients of Report Bee applications}
-  spec.homepage      = "https://www.reportbee.com/"
+  spec.homepage      = 'https://www.reportbee.com/'
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "https://reportbee.codebasehq.com/projects/reportbee/repositories/omniauth-reportbee"
+    spec.metadata['allowed_push_host'] = 'https://reportbee.codebasehq.com/projects/reportbee/repositories/omniauth-reportbee'
   else
-    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against public gem pushes.'
   end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.10"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_dependency "omniauth-oauth2", ["= 1.4.0"]
-  spec.add_dependency "json"
-  spec.add_dependency "rest-client"
+  spec.add_development_dependency 'bundler', '~> 1.10'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_dependency 'omniauth-oauth2', ['= 1.4.0']
+  spec.add_dependency 'json'
+  spec.add_dependency 'rest-client'
+  spec.add_dependency 'airbrake', '~> 5.4'
 end
